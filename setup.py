6853bcf266a695fd4d9db9723aaf5a1638a5851d
faizraza <razafaiz003@gmail.com>
2025-07-23 21:47:40 +0530

    logging & exception

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ version='0.0.1',
 author='faiz',
 author_email='razafaiz003@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+install_requires=get_requirements('requirments.txt')
 
 )
